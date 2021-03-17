267bed6414d12c3a9099dc30bcaef95ac6469e18
Kr0n0 <ccvals@linux.com>
2021-03-17 20:16:25 +0100

    Added Jupyterlab docker build scripts

--- k3s/tensorflow-jupyterlab/build_docker.sh
+++ k3s/tensorflow-jupyterlab/build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-NAME=xift/l4t-tensorflow-jupyterlab:latest
+NAME=xift/l4t-tensorflow-jupyterlab:r32.5.0-tf2.3.1-py3
 docker build -t ${NAME} . -f Dockerfile.jupyterlab
---
+++ k3s/tensorflow-jupyterlab/push_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker login
+docker push xift/l4t-tensorflow-jupyterlab:r32.5.0-tf2.3.1-py3
